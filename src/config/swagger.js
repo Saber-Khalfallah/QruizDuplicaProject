0f822abcfd456741e2ea74d1e2a4390bc3a4bb77
Saber khalfallah <sabeur.khalfallah94@gmail.com>
2025-01-13 17:47:35 +0100

    Swagger Adjustement

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -4,19 +4,29 @@ const swaggerOptions = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'Your API Documentation',
+            title: 'Qruiz API Documentation',
             version: '1.0.0',
             description: 'Automatically generated API documentation using Swagger',
         },
         servers: [
             {
-                url: 'https://qruizduplicaproject.onrender.com/', // Replace with your actual base URL
-                
+                url: 'https://qruizduplicaproject.onrender.com/', 
             },
         ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                    description: 'Enter your JWT token in the format: Bearer <token>',
+                },
+            },
+        },
     },
     apis: ['./src/routes/*.js'], // Path to your route files
 };
 
 const specs = swaggerJsdoc(swaggerOptions);
+
 module.exports = specs;
